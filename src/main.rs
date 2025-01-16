c28d4e4b46812e385a58ff9dea2b4302eda07c2b
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-01-16 20:38:25 +0530

    dev: remove unused deps

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+use std::cell::RefCell;
 use std::hint::black_box;
 use std::process::exit;
+use std::rc::Rc;
+use std::time::Instant;
 
-use fuel_vm::fuel_asm::{op, RawInstruction};
+use fuel_vm::fuel_asm::op;
 use fuel_vm::interpreter::InterpreterParams;
-use fuel_vm::prelude::{
-    Finalizable, GasCosts, Instruction, IntoChecked, RegId, Script, TransactionBuilder,
-};
+use fuel_vm::prelude::{Finalizable, GasCosts, IntoChecked, RegId, Script, TransactionBuilder};
 use fuel_vm::state::ExecuteState;
 use fuel_vm::storage::MemoryStorage;
 use fuel_vm::{interpreter::Interpreter, prelude::MemoryInstance};
+use revm::context::{BlockEnv, CfgEnv, TxEnv};
+use revm::interpreter::interpreter::EthInterpreter;
+
+use revm::interpreter::table::make_instruction_table;
+use revm::interpreter::{DummyHost, InputsImpl, Interpreter as RevmInterpreter, SharedMemory};
+use revm::primitives::{Address as RevmAddress, Bytes, U256 as RevmU256};
+use revm::specification::hardfork::SpecId;
+use revm::state::Bytecode;
 
 fn main() {
     let mut interpreter = Interpreter::<_, _, Script>::with_storage(
@@ -20,17 +29,26 @@ fn main() {
         },
     );
 
-    let number_of_iterations = 1000;
-    let result_register = RegId::new(19);
+    let a: u32 = 100000;
+    let b: u32 = 10000;
+    let number_of_iterations: u64 = (a * b).into();
+
+    println!("number_of_iterations: {:?}", number_of_iterations);
+
+    let reg_a = RegId::new(19);
+    let reg_b = RegId::new(20);
+    let reg_result = RegId::new(21);
 
     let script = TransactionBuilder::script(
         vec![
-            op::addi(RegId::WRITABLE, RegId::ZERO, number_of_iterations),
-            op::addi(result_register, RegId::ZERO, 0),
-            op::addi(result_register, result_register, 1),
+            op::movi(reg_a, a),
+            op::movi(reg_b, b),
+            op::mul(RegId::WRITABLE, reg_a, reg_b),
+            op::addi(reg_result, RegId::ZERO, 0),
+            op::addi(reg_result, reg_result, 1),
             op::subi(RegId::WRITABLE, RegId::WRITABLE, 1),
             op::jnzb(RegId::WRITABLE, RegId::ONE, 0),
-            op::ret(result_register),
+            op::ret(reg_result),
         ]
         .into_iter()
         .collect(),
@@ -46,26 +64,7 @@ fn main() {
     let script = script.test_into_ready();
     black_box(interpreter.init_script(script)).unwrap();
 
-    // Instruction set pointer
-    let is = interpreter.registers().get(12).unwrap().clone();
-    println!("{:?}", is);
-
-    // Instruction set pointer
-    let pc = interpreter.registers().get(3).unwrap().clone();
-    println!("{:?}", pc);
-
-    // let's read value there
-    let value: [u8; 4] = interpreter.memory().read_bytes(is).unwrap();
-    println!("value at memory {:?}", value);
-
-    let instruction = Instruction::try_from(value).unwrap();
-    println!("value at memory {:?}", instruction);
-
-    let value: [u8; 4] = interpreter.memory().read_bytes(is + 4).unwrap();
-    println!("value at memory {:?}", value);
-
-    let instruction = Instruction::try_from(value).unwrap();
-    println!("value at memory {:?}", instruction);
+    let start = Instant::now();
 
     let result = loop {
         let script = interpreter.execute().unwrap();
@@ -82,5 +81,79 @@ fn main() {
         }
     };
 
-    println!("result: {:?}", result);
+    let duration = start.elapsed();
+    println!("addition result on FuelVM: {:?}", result);
+    println!("Time elapsed on FuelVM: {:?}", duration);
+
+    // convert number of iterations to bytes, little endian, padded to 8 bytes
+    let number_of_iterations_bytes = number_of_iterations.to_be_bytes();
+    // println!("number_of_iterations_bytes: {:?}", number_of_iterations_bytes);
+
+    let bytecode = [
+        0x67,
+        number_of_iterations_bytes[0],
+        number_of_iterations_bytes[1],
+        number_of_iterations_bytes[2],
+        number_of_iterations_bytes[3],
+        number_of_iterations_bytes[4],
+        number_of_iterations_bytes[5],
+        number_of_iterations_bytes[6],
+        number_of_iterations_bytes[7], // PUSH8 number_of_iterations
+        0x60,
+        0x00, // PUSH1 0x00
+        0x5b, // JUMPDEST
+        0x81, // DUP2
+        0x15, // ISZERO
+        0x60,
+        0x1d, // PUSH1 0x1d
+        0x57, // JUMPI
+        0x60,
+        0x01, // PUSH1 0x01
+        0x01, // ADD
+        0x90, // SWAP1
+        0x60,
+        0x01, // PUSH1 0x01
+        0x90, // SWAP1
+        0x03, // SUB
+        0x90, // SWAP1
+        0x60,
+        0x0b, // PUSH1 0x0b
+        0x56, // JUMP
+        0x5b, // JUMPDEST
+        0x90, // SWAP1
+        0x50, // POP
+    ]
+    .to_vec();
+
+    // reth side of the code
+    // ---------
+    // the bytecode needs to the following:
+    // iterate a certain number of times, and keep adding 1 to the result which is initially zero
+    let bytecode = Bytecode::new_raw(Bytes::from(bytecode));
+
+    let mut interpreter = RevmInterpreter::<EthInterpreter>::new(
+        Rc::new(RefCell::new(SharedMemory::new())),
+        bytecode,
+        InputsImpl {
+            target_address: RevmAddress::ZERO,
+            caller_address: RevmAddress::ZERO,
+            input: Bytes::default(),
+            call_value: RevmU256::ZERO,
+        },
+        false,
+        false,
+        SpecId::LATEST,
+        u64::MAX,
+    );
+
+    let mut host = DummyHost::<BlockEnv, TxEnv, CfgEnv>::default();
+
+    let table = make_instruction_table::<EthInterpreter, DummyHost<BlockEnv, TxEnv, CfgEnv>>();
+
+    let start = Instant::now();
+    interpreter.run(&table, &mut host);
+    let duration = start.elapsed();
+    println!("Time elapsed on rEVM: {:?}", duration);
+
+    println!("result on rEVM: {:?}", interpreter.stack.pop());
 }
